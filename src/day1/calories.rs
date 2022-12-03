03b85e5829408169a40478918c6cb59c70ee5528
Emilio Ziniades <emilioziniades@protonmail.com>
2022-12-03 14:07:09 +0200

    day 1 complete

---
+++ src/day1/calories.rs
@@ -0,0 +1,47 @@
+use std::fs;
+
+pub fn max_calories(file: &str, n_elves: usize) -> i32 {
+    let data = fs::read_to_string(file).unwrap();
+    let mut calories = data
+        .trim()
+        .split("\n\n")
+        .map(|x| {
+            x.trim()
+                .split('\n')
+                .map(|x| x.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect::<Vec<i32>>();
+
+    calories.sort_by(|a, b| b.cmp(a));
+    calories.truncate(n_elves);
+    calories.into_iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::fetch;
+
+    fn init() {
+        fetch::input(1);
+    }
+
+    #[test]
+    fn max_calories_elf() {
+        init();
+
+        let tests = vec![
+            ("src/day1/example.txt", 24000, 1),
+            ("src/day1/example.txt", 45000, 3),
+            ("src/day1/input.txt", 69693, 1),
+            ("src/day1/input.txt", 200945, 3),
+        ];
+
+        for test in tests {
+            let (file, want, n_elves) = test;
+            let got = max_calories(file, n_elves);
+            assert_eq!(got, want, "got {got}, wanted {want}")
+        }
+    }
+}
