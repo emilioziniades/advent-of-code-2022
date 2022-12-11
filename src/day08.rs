e5a9c6509b4abdaa06f016a0436d5041d0665875
Emilio Ziniades <emilioziniades@protonmail.com>
2022-12-11 23:08:32 +0200

    day 8 part 2

--- src/day08.rs
+++ src/day08.rs
@@ -22,14 +22,40 @@ impl Grid {
         self.0[pt.x][pt.y]
     }
 
-    fn is_visible(&self, pt: &Point) -> bool {
-        self.is_visible_from(pt, Direction::Left)
-            || self.is_visible_from(pt, Direction::Right)
-            || self.is_visible_from(pt, Direction::Up)
-            || self.is_visible_from(pt, Direction::Down)
+    fn scenic_score(&self, pt: &Point) -> u32 {
+        self.scenic_score_from(pt, &Direction::Left)
+            * self.scenic_score_from(pt, &Direction::Right)
+            * self.scenic_score_from(pt, &Direction::Up)
+            * self.scenic_score_from(pt, &Direction::Down)
     }
 
-    fn is_visible_from(&self, pt: &Point, direction: Direction) -> bool {
+    fn scenic_score_from(&self, pt: &Point, direction: &Direction) -> u32 {
+        let val = self.at(pt);
+        let mut items_until = self.items_until(pt, direction);
+
+        match direction {
+            Direction::Left | Direction::Up => items_until.reverse(),
+            _ => (),
+        }
+
+        let mut visible_items = 0;
+        for item in items_until {
+            visible_items += 1;
+            if item >= &val {
+                break;
+            }
+        }
+        visible_items
+    }
+
+    fn is_visible(&self, pt: &Point) -> bool {
+        self.is_visible_from(pt, &Direction::Left)
+            || self.is_visible_from(pt, &Direction::Right)
+            || self.is_visible_from(pt, &Direction::Up)
+            || self.is_visible_from(pt, &Direction::Down)
+    }
+
+    fn is_visible_from(&self, pt: &Point, direction: &Direction) -> bool {
         let val = self.at(pt);
         let max_val = self.items_until(pt, direction).into_iter().max();
         match max_val {
@@ -38,7 +64,7 @@ impl Grid {
         }
     }
 
-    fn items_until(&self, pt: &Point, direction: Direction) -> Vec<&u8> {
+    fn items_until(&self, pt: &Point, direction: &Direction) -> Vec<&u8> {
         let Point { x, y } = *pt;
         match direction {
             Direction::Left => self.0.get(x).unwrap().iter().take(y).collect(),
@@ -77,10 +103,21 @@ pub fn count_visible_trees(filename: &str) -> u32 {
     visible_count
 }
 
-// pub fn max_scenic_score(filename: &str) -> u32 {
-//     let grid = Grid::from(filename);
-//     42
-// }
+pub fn max_scenic_score(filename: &str) -> u32 {
+    let grid = Grid::from(filename);
+    let positions: Vec<Point> = grid
+        .0
+        .iter()
+        .enumerate()
+        .flat_map(|(x, row)| row.iter().enumerate().map(move |(y, _)| Point { x, y }))
+        .collect();
+
+    positions
+        .iter()
+        .map(|position| grid.scenic_score(position))
+        .max()
+        .unwrap()
+}
 
 #[cfg(test)]
 mod tests {
@@ -99,16 +136,16 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn max_scenic_score() {
-    //     fetch_input(8);
-    //
-    //     let tests = vec![("example/day08.txt", 8), ("input/day08.txt", 0)];
-    //
-    //     for test in tests {
-    //         let (filename, want) = test;
-    //         let got = day08::max_scenic_score(filename);
-    //         assert_eq!(got, want, "got {got}, wanted {want}, for {filename}")
-    //     }
-    // }
+    #[test]
+    fn max_scenic_score() {
+        fetch_input(8);
+
+        let tests = vec![("example/day08.txt", 8), ("input/day08.txt", 374400)];
+
+        for test in tests {
+            let (filename, want) = test;
+            let got = day08::max_scenic_score(filename);
+            assert_eq!(got, want, "got {got}, wanted {want}, for {filename}")
+        }
+    }
 }
