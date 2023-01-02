88ea3e46cdb74231564d8c853b9f84f0192dd70b
Emilio Ziniades <emilioziniades@protonmail.com>
2023-01-02 15:13:55 +0200

    day 13 part 1

--- src/day13.rs
+++ src/day13.rs
@@ -1,6 +1,6 @@
 use std::{cmp, cmp::Ordering, fs};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum Packet {
     List(Vec<Packet>),
     Number(u32),
@@ -16,6 +16,73 @@ impl Packet {
         parse(&mut tokens, &mut root);
 
         root
+    }
+
+    fn partial_cmp_recursive(&self, other: &Self, exit: &mut bool, order: &mut Option<Ordering>) {
+        if *exit {
+            return;
+        }
+        match (self, other) {
+            (Packet::List(left), Packet::List(right)) => {
+                let max_length = cmp::max(left.len(), right.len());
+                for i in 0..max_length {
+                    if *exit {
+                        return;
+                    }
+
+                    let left = left.get(i);
+                    let right = right.get(i);
+
+                    match (left, right) {
+                        (Some(left), Some(right)) => left.partial_cmp_recursive(right, exit, order),
+                        (Some(_left), None) => {
+                            // right ran out of items first
+                            *order = Some(Ordering::Greater);
+                            *exit = true;
+                            break;
+                        }
+                        (None, Some(_right)) => {
+                            // left ran out of items first
+                            *order = Some(Ordering::Less);
+                            *exit = true;
+                            break;
+                        }
+                        (None, None) => panic!("exceeded both left and right vec lengths"),
+                    }
+                }
+            }
+
+            (Packet::Number(left), Packet::Number(right)) => match left.cmp(right) {
+                Ordering::Less => {
+                    *order = Some(Ordering::Less);
+                    *exit = true;
+                }
+                Ordering::Greater => {
+                    *order = Some(Ordering::Greater);
+                    *exit = true;
+                }
+                Ordering::Equal => {}
+            },
+
+            (Packet::List(_), Packet::Number(_)) => {
+                let other = &Packet::List(vec![other.clone()]);
+                self.partial_cmp_recursive(other, exit, order)
+            }
+
+            (Packet::Number(_), Packet::List(_)) => {
+                let left = &Packet::List(vec![self.clone()]);
+                left.partial_cmp_recursive(other, exit, order)
+            }
+        }
+    }
+}
+
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let mut exit = false;
+        let mut order = None;
+        self.partial_cmp_recursive(other, &mut exit, &mut order);
+        order
     }
 }
 
@@ -65,72 +132,8 @@ fn tokenize(line: &str) -> Vec<String> {
             n => current_number.push(n),
         }
     }
-    tokens.reverse(); // so that we can "pop" off front
+    tokens.reverse(); // so that we can pop off front
     tokens
-}
-
-fn compare(left: &Packet, right: &Packet) -> Ordering {
-    let mut exit = false;
-    let mut order = Ordering::Equal;
-    compare_recursive(left, right, &mut exit, &mut order);
-
-    println!("\n>>>>>>>>>>>>>>>>>>>>>>>>>  {order:?}\n");
-
-    order
-}
-fn compare_recursive(left: &Packet, right: &Packet, exit: &mut bool, order: &mut Ordering) {
-    if *exit {
-        return;
-    }
-    match (left, right) {
-        (Packet::List(left), Packet::List(right)) => {
-            let max_length = cmp::max(left.len(), right.len());
-            for i in 0..max_length {
-                if *exit {
-                    return;
-                }
-
-                let left = dbg!(left.get(i));
-                let right = dbg!(right.get(i));
-
-                match (left, right) {
-                    (Some(left), Some(right)) => compare_recursive(left, right, exit, order),
-                    (Some(_left), None) => {
-                        // right ran out of items first
-                        *order = Ordering::Greater;
-                        *exit = true;
-                        break;
-                    }
-                    (None, Some(_right)) => {
-                        // left ran out of items first
-                        *order = Ordering::Less;
-                        *exit = true;
-                        break;
-                    }
-                    (None, None) => panic!("exceeded both left and right vec lengths"),
-                }
-            }
-        }
-        (Packet::Number(l), Packet::Number(r)) => match dbg!(dbg!(l).cmp(dbg!(r))) {
-            Ordering::Equal => {}
-            Ordering::Less => {
-                *order = Ordering::Less;
-                *exit = true;
-            }
-            Ordering::Greater => {
-                *order = Ordering::Greater;
-                *exit = true;
-            }
-        },
-        (Packet::List(_), Packet::Number(_)) => {
-            let right = &Packet::List(vec![right.clone()]);
-            compare_recursive(left, right, exit, order)
-        }
-        (Packet::Number(_), Packet::List(_)) => {
-            let left = &Packet::List(vec![left.clone()]);
-            compare_recursive(left, right, exit, order)
-        }
-    }
 }
 
 pub fn sum_ordered_pairs(filename: &str) -> usize {
@@ -143,15 +146,14 @@ pub fn sum_ordered_pairs(filename: &str) -> usize {
         .map(|(pair, index)| {
             let (left, right) = {
                 let (left, right) = pair.split_once('\n').unwrap();
-                (dbg!(Packet::from(left)), dbg!(Packet::from(right)))
-                // (Packet::from(left), Packet::from(right))
+                (Packet::from(left), Packet::from(right))
             };
 
-            dbg!(match compare(&left, &right) {
-                Ordering::Less => index,
-                Ordering::Greater => 0,
-                Ordering::Equal => panic!("packets will never be equal"),
-            })
+            if left < right {
+                index
+            } else {
+                0
+            }
         })
         .sum()
 }
