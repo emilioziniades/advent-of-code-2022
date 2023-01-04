df66812fc23d0a9c5b1d69c4db78bd80438f7d4b
Emilio Ziniades <emilioziniades@protonmail.com>
2023-01-04 22:57:01 +0200

    day 15 part 1 faster

--- src/day15.rs
+++ src/day15.rs
@@ -1,10 +1,10 @@
-use std::{cmp::Ordering, fs, ops::RangeInclusive};
+use std::{collections::HashSet, fs, ops::RangeInclusive};
 
 const CHARACTERS: [char; 17] = [
     'S', 'e', 'n', 's', 'o', 'r', 'a', 't', 'c', 'l', 'b', 'i', 'x', 'y', '=', ':', ',',
 ];
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct Point {
     x: i32,
     y: i32,
@@ -43,7 +43,9 @@ pub fn count_non_beacons(file: &str, row: i32) -> usize {
     let beacons_in_row = sensors
         .iter()
         .filter(|sensor| sensor.beacon.y == row)
-        .count();
+        .map(|sensor| sensor.beacon)
+        .collect::<HashSet<Point>>()
+        .len();
 
     let mut ranges: Vec<RangeInclusive<i32>> = Vec::new();
 
@@ -60,49 +62,34 @@ pub fn count_non_beacons(file: &str, row: i32) -> usize {
         }
     }
 
-    ranges = remove_overlapping_ranges(&mut ranges);
-
-    println!("{ranges:?}");
+    let ranges = merge_intervals(&mut ranges);
 
     let point_count: usize = ranges.into_iter().map(|range| range.count()).sum();
 
-    point_count - beacons_in_row - 2
+    point_count - beacons_in_row
 }
 
-fn manhattan_distance(a: &Point, b: &Point) -> u32 {
-    a.x.abs_diff(b.x) + a.y.abs_diff(b.y)
-}
+fn merge_intervals(ranges: &mut [RangeInclusive<i32>]) -> Vec<RangeInclusive<i32>> {
+    ranges.sort_by_key(|range| *range.start());
 
-fn remove_overlapping_ranges(ranges: &mut Vec<RangeInclusive<i32>>) -> Vec<RangeInclusive<i32>> {
-    ranges.sort_by(compare_ranges);
-    println!("{ranges:?}");
-    ranges.reverse();
+    let mut result: Vec<RangeInclusive<i32>> = vec![ranges[0].clone()];
 
-    let mut result: Vec<RangeInclusive<i32>> = Vec::new();
-
-    while let Some((first, second)) = ranges.pop().zip(ranges.pop()) {
-        // first wholy contains second
-        if first.start() < second.start() && first.end() > second.end() {
-            result.push(first);
-        }
-        // second's start is in first
-        else if first.start() < second.start() && first.end() > second.start() {
-            result.push(*first.start()..=*second.start());
-            result.push(*second.start() + 1..=*first.end());
-            result.push(*first.end() + 1..=*second.end());
-        }
-        // no overlap
-        else {
-            result.push(first);
-            result.push(second);
+    for range in ranges.iter_mut().skip(1) {
+        let last_range = result.pop().unwrap();
+        if range.start() <= last_range.end() {
+            let new_end = last_range.end().max(range.end());
+            result.push(*last_range.start()..=*new_end);
+        } else {
+            result.push(last_range);
+            result.push(range.clone());
         }
     }
 
     result
 }
 
-fn compare_ranges(a: &RangeInclusive<i32>, b: &RangeInclusive<i32>) -> Ordering {
-    a.start().cmp(b.start())
+fn manhattan_distance(a: &Point, b: &Point) -> u32 {
+    a.x.abs_diff(b.x) + a.y.abs_diff(b.y)
 }
 
 #[cfg(test)]
@@ -115,7 +102,7 @@ mod tests {
 
         let tests = vec![
             ("example/day15.txt", 26, 10),
-            // ("input/day15.txt", 5_125_700, 2_000_000),
+            ("input/day15.txt", 5_125_700, 2_000_000),
         ];
 
         for test in tests {
