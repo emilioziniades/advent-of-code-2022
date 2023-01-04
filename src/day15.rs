198326c05060a0a16bdfb13fee5dd47b7b33efe9
Emilio Ziniades <emilioziniades@protonmail.com>
2023-01-04 15:29:05 +0200

    day15 speed up

--- src/day15.rs
+++ src/day15.rs
@@ -1,10 +1,10 @@
-use std::{collections::HashSet, fs};
+use std::{cmp::Ordering, fs, ops::RangeInclusive};
 
 const CHARACTERS: [char; 17] = [
     'S', 'e', 'n', 's', 'o', 'r', 'a', 't', 'c', 'l', 'b', 'i', 'x', 'y', '=', ':', ',',
 ];
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Copy)]
 struct Point {
     x: i32,
     y: i32,
@@ -17,8 +17,8 @@ struct Sensor {
 }
 
 pub fn count_non_beacons(file: &str, row: i32) -> usize {
-    let lines = fs::read_to_string(file).unwrap();
-    let sensors: Vec<Sensor> = lines
+    let input = fs::read_to_string(file).unwrap();
+    let sensors: Vec<Sensor> = input
         .lines()
         .map(|line| {
             let numbers = line
@@ -40,7 +40,12 @@ pub fn count_non_beacons(file: &str, row: i32) -> usize {
         })
         .collect();
 
-    let mut positions: HashSet<Point> = HashSet::new();
+    let beacons_in_row = sensors
+        .iter()
+        .filter(|sensor| sensor.beacon.y == row)
+        .count();
+
+    let mut ranges: Vec<RangeInclusive<i32>> = Vec::new();
 
     for sensor in sensors {
         // does it reach row
@@ -51,25 +56,53 @@ pub fn count_non_beacons(file: &str, row: i32) -> usize {
             let vert_distance = (beacon_distance - row_distance) as i32;
             let row_start = sensor.position.x - vert_distance;
             let row_end = sensor.position.x + vert_distance;
-            positions.extend((row_start..=row_end).map(|x| Point { x, y: row }));
-
-            if sensor.beacon.y == row {
-                positions.remove(&sensor.beacon);
-            }
+            ranges.push(row_start..=row_end);
         }
     }
 
-    let mut xs = positions.iter().map(|pt| pt.x).collect::<Vec<i32>>();
-    xs.sort();
+    ranges = remove_overlapping_ranges(&mut ranges);
 
-    println!("{xs:#?}");
-    println!("{}", xs.len());
+    println!("{ranges:?}");
 
-    positions.len()
+    let point_count: usize = ranges.into_iter().map(|range| range.count()).sum();
+
+    point_count - beacons_in_row - 2
 }
 
 fn manhattan_distance(a: &Point, b: &Point) -> u32 {
     a.x.abs_diff(b.x) + a.y.abs_diff(b.y)
+}
+
+fn remove_overlapping_ranges(ranges: &mut Vec<RangeInclusive<i32>>) -> Vec<RangeInclusive<i32>> {
+    ranges.sort_by(compare_ranges);
+    println!("{ranges:?}");
+    ranges.reverse();
+
+    let mut result: Vec<RangeInclusive<i32>> = Vec::new();
+
+    while let Some((first, second)) = ranges.pop().zip(ranges.pop()) {
+        // first wholy contains second
+        if first.start() < second.start() && first.end() > second.end() {
+            result.push(first);
+        }
+        // second's start is in first
+        else if first.start() < second.start() && first.end() > second.start() {
+            result.push(*first.start()..=*second.start());
+            result.push(*second.start() + 1..=*first.end());
+            result.push(*first.end() + 1..=*second.end());
+        }
+        // no overlap
+        else {
+            result.push(first);
+            result.push(second);
+        }
+    }
+
+    result
+}
+
+fn compare_ranges(a: &RangeInclusive<i32>, b: &RangeInclusive<i32>) -> Ordering {
+    a.start().cmp(b.start())
 }
 
 #[cfg(test)]
@@ -82,7 +115,7 @@ mod tests {
 
         let tests = vec![
             ("example/day15.txt", 26, 10),
-            ("input/day15.txt", 5_125_700, 2_000_000),
+            // ("input/day15.txt", 5_125_700, 2_000_000),
         ];
 
         for test in tests {
