60638a4a5e55887707ade30fb7dbbf226a647f5c
Emilio Ziniades <emilioziniades@protonmail.com>
2023-04-17 23:01:17 +0200

    day 16 part 2 example working but slow for input

--- src/day16.rs
+++ src/day16.rs
@@ -7,13 +7,14 @@
  *    then don't pursue it.
  */
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     fs,
 };
 
 use crate::queue::PriorityQueue;
 
 const MAX_MINUTES: usize = 30;
+const MAX_MINUTES_WITH_ELEPHANT: usize = 26;
 const START: Valve = ['A', 'A'];
 
 type Distances<'a> = HashMap<&'a Valve, HashMap<&'a Valve, usize>>;
@@ -29,8 +30,8 @@ impl<'a> NestedGetter<Valve, usize> for Distances<'a> {
 }
 
 #[derive(Debug)]
-struct ValveOpenBenefit<'a> {
-    target_valve: &'a Valve,
+struct ValveOpenBenefit {
+    target_valve: Valve,
     current_minute: usize,
     benefit: usize,
 }
@@ -127,27 +128,28 @@ impl Volcano {
         distances
     }
 
-    fn benefit_from_opening_valve<'a>(
+    fn benefit_from_opening_valve(
         &self,
-        visited: &HashSet<&Valve>,
-        current_valve: &'a Valve,
-        target_valve: &'a Valve,
+        visited: &HashSet<Valve>,
+        current_valve: Valve,
+        target_valve: Valve,
         current_minute: usize,
         pairwise_distances: &Distances,
-    ) -> Option<ValveOpenBenefit<'a>> {
+        max_minutes: usize,
+    ) -> Option<ValveOpenBenefit> {
         if visited.contains(&target_valve) {
             return None;
         }
 
-        let minutes_travelled = pairwise_distances.get_nested(current_valve, target_valve)?;
+        let minutes_travelled = pairwise_distances.get_nested(&current_valve, &target_valve)?;
         let first_minute_of_pressure_release = current_minute + minutes_travelled + 1; // include 1 minute to open
 
-        if first_minute_of_pressure_release > MAX_MINUTES {
+        if first_minute_of_pressure_release > max_minutes {
             return None;
         }
 
-        let flow_rate = self.flow_rate(target_valve);
-        let minutes_of_pressure_release = MAX_MINUTES - first_minute_of_pressure_release + 1; // inclusive counting
+        let flow_rate = self.flow_rate(&target_valve);
+        let minutes_of_pressure_release = max_minutes - first_minute_of_pressure_release + 1; // inclusive counting
         let benefit = flow_rate * minutes_of_pressure_release;
 
         let valve_open_benefit = ValveOpenBenefit {
@@ -159,12 +161,13 @@ impl Volcano {
         Some(valve_open_benefit)
     }
 
-    fn benefit_from_opening_all_valves<'a>(
-        &'a self,
-        visited: &HashSet<&Valve>,
-        current_valve: &'a Valve,
+    fn benefit_from_opening_all_valves(
+        &self,
+        visited: &HashSet<Valve>,
+        current_valve: Valve,
         current_minute: usize,
         pairwise_distances: &Distances,
+        max_minutes: usize,
     ) -> Vec<ValveOpenBenefit> {
         self.valves()
             .into_iter()
@@ -172,15 +175,16 @@ impl Volcano {
                 self.benefit_from_opening_valve(
                     visited,
                     current_valve,
-                    target_valve,
+                    *target_valve,
                     current_minute,
                     pairwise_distances,
+                    max_minutes,
                 )
             })
             .collect()
     }
 
-    fn prune_graph(&mut self) {
+    fn pruned(mut self) -> Self {
         let valves_to_prune: Vec<&Valve> = self
             .flow_rates
             .iter()
@@ -207,12 +211,24 @@ impl Volcano {
                 }
             }
         }
+
+        self
     }
 }
 
+#[derive(Debug, Clone)]
+struct TeamRoute {
+    visited: HashSet<Valve>,
+    benefit: usize,
+    human_current_valve: Valve,
+    human_minute: usize,
+    elephant_current_valve: Valve,
+    elephant_minute: usize,
+}
+
 fn find_all_routes(volcano: &Volcano, pairwise_distances: &Distances) -> Vec<usize> {
-    let visited = HashSet::from([&START]);
-    let current_valve = &START;
+    let visited = HashSet::from([START]);
+    let current_valve = START;
     let start_minute = 1;
     let start_benefit = 0;
     let initial_possible_next_moves = volcano.benefit_from_opening_all_valves(
@@ -220,6 +236,7 @@ fn find_all_routes(volcano: &Volcano, pairwise_distances: &Distances) -> Vec<usi
         current_valve,
         start_minute,
         pairwise_distances,
+        MAX_MINUTES,
     );
     find_all_routes_recursive(
         &visited,
@@ -227,15 +244,17 @@ fn find_all_routes(volcano: &Volcano, pairwise_distances: &Distances) -> Vec<usi
         volcano,
         pairwise_distances,
         initial_possible_next_moves,
+        MAX_MINUTES,
     )
 }
 
 fn find_all_routes_recursive(
-    visited: &HashSet<&Valve>,
+    visited: &HashSet<Valve>,
     benefit: usize,
     volcano: &Volcano,
     pairwise_distances: &Distances,
     next_possible_benefits: Vec<ValveOpenBenefit>,
+    max_minutes: usize,
 ) -> Vec<usize> {
     next_possible_benefits
         .iter()
@@ -248,6 +267,7 @@ fn find_all_routes_recursive(
                 valve_open_benefit.target_valve,
                 valve_open_benefit.current_minute,
                 pairwise_distances,
+                max_minutes,
             );
 
             if next_possible_valve_openings.is_empty() {
@@ -259,6 +279,7 @@ fn find_all_routes_recursive(
                     volcano,
                     pairwise_distances,
                     next_possible_valve_openings,
+                    max_minutes,
                 )
             }
         })
@@ -266,8 +287,7 @@ fn find_all_routes_recursive(
 }
 
 pub fn maximize_pressure_release(filename: &str) -> usize {
-    let mut volcano = Volcano::from_file(filename);
-    volcano.prune_graph();
+    let volcano = Volcano::from_file(filename).pruned();
     let pairwise_distances = volcano.compute_pairwise_distances();
     find_all_routes(&volcano, &pairwise_distances)
         .into_iter()
@@ -275,11 +295,102 @@ pub fn maximize_pressure_release(filename: &str) -> usize {
         .unwrap()
 }
 
+pub fn maximize_pressure_release_with_elephant(filename: &str) -> usize {
+    let volcano = Volcano::from_file(filename).pruned();
+    let pairwise_distances = volcano.compute_pairwise_distances();
+
+    let mut possible_routes: VecDeque<TeamRoute> = VecDeque::new();
+    possible_routes.push_front(TeamRoute {
+        visited: HashSet::from([START]),
+        benefit: 0,
+        human_current_valve: START,
+        human_minute: 1,
+        elephant_current_valve: START,
+        elephant_minute: 1,
+    });
+
+    let mut max_benefit = 0;
+
+    while let Some(team_route) = possible_routes.pop_back() {
+        // what happens if we open all the valves right now
+        /* let maximum_possible_benefit: usize = volcano
+            .flow_rates
+            .iter()
+            .map(|(valve, _)| {
+                let benefit = volcano.benefit_from_opening_valve(
+                    &team_route.visited,
+                    *valve,
+                    *valve,
+                    min(team_route.human_minute, team_route.elephant_minute),
+                    &pairwise_distances,
+                    MAX_MINUTES_WITH_ELEPHANT,
+                );
+
+                match benefit {
+                    None => 0,
+                    Some(benefit) => benefit.benefit,
+                }
+            })
+            .sum();
+
+        if maximum_possible_benefit < max_benefit {
+            println!("purged a possibility!");
+            continue;
+        } */
+
+        println!("{team_route:?}");
+        let possible_human_benefits = volcano.benefit_from_opening_all_valves(
+            &team_route.visited,
+            team_route.human_current_valve,
+            team_route.human_minute,
+            &pairwise_distances,
+            MAX_MINUTES_WITH_ELEPHANT,
+        );
+
+        let possible_elephant_benefits = volcano.benefit_from_opening_all_valves(
+            &team_route.visited,
+            team_route.elephant_current_valve,
+            team_route.elephant_minute,
+            &pairwise_distances,
+            MAX_MINUTES_WITH_ELEPHANT,
+        );
+
+        if possible_human_benefits.is_empty() && possible_elephant_benefits.is_empty() {
+            if team_route.benefit > max_benefit {
+                max_benefit = team_route.benefit;
+            }
+            continue;
+        }
+
+        for human_benefit in possible_human_benefits {
+            for elephant_benefit in possible_elephant_benefits.iter() {
+                if human_benefit.target_valve == elephant_benefit.target_valve {
+                    continue;
+                }
+
+                let mut team_route = team_route.clone();
+                team_route.visited.insert(human_benefit.target_valve);
+                team_route.visited.insert(elephant_benefit.target_valve);
+                team_route.human_current_valve = human_benefit.target_valve;
+                team_route.elephant_current_valve = elephant_benefit.target_valve;
+                team_route.human_minute = human_benefit.current_minute;
+                team_route.elephant_minute = elephant_benefit.current_minute;
+                team_route.benefit += human_benefit.benefit + elephant_benefit.benefit;
+
+                possible_routes.push_front(team_route);
+            }
+        }
+    }
+
+    max_benefit
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{day16, fetch_input};
 
     #[test]
+    // #[ignore]
     fn maximize_pressure_release() {
         fetch_input(16);
 
@@ -290,6 +401,22 @@ mod tests {
         for test in tests {
             let (file, want) = test;
             let got = day16::maximize_pressure_release(file);
+            assert_eq!(got, want, "got {got}, wanted {want}")
+        }
+    }
+
+    #[test]
+    // #[ignore]
+    fn maximize_pressure_release_with_elephant() {
+        fetch_input(16);
+
+        let tests = vec![("example/day16.txt", 1707)];
+        // let tests = vec![("input/day16.txt", 0)];
+        // let tests = vec![("example/day16.txt", 1707), ("input/day16.txt", 0)];
+
+        for test in tests {
+            let (file, want) = test;
+            let got = day16::maximize_pressure_release_with_elephant(file);
             assert_eq!(got, want, "got {got}, wanted {want}")
         }
     }
