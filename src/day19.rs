b408ebf15f3d88a8635cd0ee0273e7d571f7c0ef
Emilio Ziniades <emilioziniades@protonmail.com>
2023-11-01 23:44:35 +0200

    day19 part 2 complete!

--- src/day19.rs
+++ src/day19.rs
@@ -1,3 +1,16 @@
+// Credit where credit is due: https://todd.ginsberg.com/post/advent-of-code/2022/day19/
+// I was getting lost in the combinations again. What this article showed me was that you
+// don't need to consider any *intermediate* states. Just consider states that changed.
+// Practically this means branching at every possible build opportunity, instead of branching
+// at every single time step. This means 'wait' no longer becomes an action, and the branching
+// factor is much much smaller. That's the big idea, and then there are a few smaller
+// optimizations, like knowing when to stop producing a certain kind of robot, since you can only
+// ever make one robot a minute. Another optimization is generating an upper bound on each
+// estimate. Generally, the lesson I took from this one, after being stuck on it for way too long,
+// stubbornly, before seeking out some clues, is 1) your initial instinct is generally correct,
+// 2) the algorithm is not so complicated - you aren't going to find the magic answer in a journal
+// article from 2007 and 3) consider the domain very specifically and don't over generalize the
+// problem.
 use std::fs;
 
 #[derive(Debug)]
@@ -95,6 +108,16 @@ impl State {
         self.obsidians -= blueprint.obsidian_for_geodebot;
         self.geodebots += 1;
     }
+
+    /// build a new geodebot every minute until the end
+    fn maximum_possible_geodes(&self, minutes: isize) -> isize {
+        let minutes_left = minutes - self.elapsed_minutes;
+        self.geodes + (0..minutes_left).map(|t| t + self.geodebots).sum::<isize>()
+    }
+
+    fn projected_geodes(&self, minutes: isize) -> isize {
+        self.geodes + (minutes - self.elapsed_minutes) * self.geodebots
+    }
 }
 
 fn max_geodes(state: State, blueprint: &Blueprint, minutes: isize) -> isize {
@@ -111,9 +134,12 @@ fn max_geodes(state: State, blueprint: &Blueprint, minutes: isize) -> isize {
     let max_obsidianbots = blueprint.obsidian_for_geodebot;
 
     while let Some(state) = queue.pop() {
-        let max_possible_geodes =
-            state.geodes + (minutes - state.elapsed_minutes) * state.geodebots;
-        max_geodes = max_geodes.max(max_possible_geodes);
+        if state.maximum_possible_geodes(minutes) < max_geodes {
+            // no point in pursuing this state further
+            continue;
+        }
+
+        max_geodes = max_geodes.max(state.projected_geodes(minutes));
 
         // build orebot
         if state.orebots < max_orebots {
@@ -216,7 +242,7 @@ mod tests {
     #[test]
     fn multiply_first_three_blueprints() {
         fetch_input(19);
-        let tests = vec![("example/day19.txt", 3472), ("input/day19.txt", 0)];
+        let tests = vec![("example/day19.txt", 3472), ("input/day19.txt", 14725)];
 
         for (filename, want) in tests {
             let got = day19::multiply_first_three_blueprints(filename);
